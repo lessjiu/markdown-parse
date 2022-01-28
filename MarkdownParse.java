5a8297eb42e4c60af455c82347378575f31eb9ef
lessjiu <97648384+lessjiu@users.noreply.github.com>
2022-01-27 18:26:59 -0800

    doesn't consider imgs or links w text btwn ] & (

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,8 +18,11 @@ public class MarkdownParse {
             
             //fixes when there are no links in the .md file, as indexOf() returns -1
             if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
+            //doesn't consider image links or links that have text in between brackets and paren
+            if (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) !='!' && openParen == nextCloseBracket + 1) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
 
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         return toReturn;
