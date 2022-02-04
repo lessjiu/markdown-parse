5f6a22ac084dee90cc9d1fb5531fccbfa2239536
lessjiu <97648384+lessjiu@users.noreply.github.com>
2022-02-04 13:48:07 -0800

    fix when there are extra lines

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,6 +18,16 @@ public class MarkdownParse {
             
             //fixes when there are no links in the .md file, as indexOf() returns -1
             if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
+	    int counter = 0;
+            for (int i = openParen; i < closeParen; i++) {
+                if (markdown.charAt(i) == '\n') {
+                    counter += 1;
+                }
+            }
+            if (counter > 1){
+		    currentIndex = closeParen + 1; 
+		    continue;
+	    }
             //doesn't consider image links or links that have text in between brackets and paren
             //allows closed bracket ] in between ]___()
             if ((nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) !='!') && markdown.charAt(openParen - 1) == ']') {
