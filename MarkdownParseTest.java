1c880cd9c71f056cf893e401e630ad1ce1ec2a7e
lessjiu <97648384+lessjiu@users.noreply.github.com>
2022-02-24 20:46:39 -0800

    added tests for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -75,4 +75,28 @@ public class MarkdownParseTest {
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 
+    //added tests for lab report 4
+
+    @Test
+    public void testSnippet1() throws IOException{
+        String contents= Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        String contents= Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        String contents= Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", 
+            "https://cse.ucsd.edu/");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
 }
